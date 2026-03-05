1c1cef4832d93414f641c7f5ee925c357fe1b377
Leandro López (inkel) <leandro.lopez@grafana.com>
2026-03-05 15:17:24 -0300

    Format Terraform code

--- test.tf
+++ test.tf
@@ -1,5 +1,5 @@
 terraform {
-  require_version = ">= 1.5.1"
+  required_version = ">= 1.5.1"
 }
 
 locals {
@@ -7,7 +7,7 @@ locals {
 }
 
 variable "bar" {
-  type    = int
+  type    = number
   default = 1024
 }
 
